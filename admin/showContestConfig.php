28ff5dd7c4015662eb01f248009739369a5686a3
gongcy <gongcy@users.noreply.github.com>
2017-09-04 03:50:25 +0800

    add show contest details support

---
+++ admin/showContestConfig.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * bluebridge
+ * @author gongchenyu
+ * @since 2017/9/4 0004 2:24
+ */
+session_start();
+header("Content-type:text/html;charset=utf-8");
+if (!isset($_GET['contest_id'])) die("访问参数错误");
+$contest_id = $_GET['contest_id'];
+require_once "../DBCN.php";
+$db = new DB();
+$data['contest_id'] = $contest_id;
+$judge['contest_id'] = '=';
+list($conSql, $mapConData)  = $db->FDFields($data, 'and', $judge);
+$contest_base_info = $db->fetch('select * from contest_blue where'.$conSql, $mapConData);
+$mData1 = $db->fetchAll('select * from contest_blue_problem where contest_id='.$contest_id);
+$problem_list = "";
+foreach ($mData1 as $value) {
+    $problem_list .= $value['type'].','.$value['num'].','.$value['score'].';';
+}
+$problem_list = trim($problem_list, ' ;');
+$problem_list = array('problem_list' => $problem_list);
+$res = array_merge($contest_base_info, $problem_list);
+print json_encode($res);
