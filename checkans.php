4fa488f6fab47cdfb85d52619f93f86e79c36912
gongchenyu <gongcy126@126.com>
2019-02-24 23:28:20 +0800

    :art: fmt checkans.php

--- checkans.php
+++ checkans.php
@@ -8,10 +8,10 @@ if ($_POST) {
     require_once 'updateans.php';
     $cid = $_POST['cid'];
     $msg = updateans($_POST['submit_id'], $cid);
-//    var_dump($msg);
+    //var_dump($msg);
     //if($msg=='panding')die("panding");
     $db = new DB();
-    $rs = $db->fetch("select * from contest_blue where contest_id=".$cid);
+    $rs = $db->fetch("select * from contest_blue where contest_id=" . $cid);
     $BEGIN_TIME = $rs['start_time'];
     $END_TIME = $rs['end_time'];
     if ((strtotime($BEGIN_TIME) < $current_time && $current_time < strtotime($END_TIME)) && $MODE == 1 && !isset($_SESSION['administrator'])) {
