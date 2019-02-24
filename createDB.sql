3e04a4f1a553f6783e062e600235a8c513116590
gongchenyu <gongcy126@126.com>
2019-02-24 22:36:19 +0800

    :wrench: change db script

--- createDB.sql
+++ createDB.sql
@@ -9,11 +9,11 @@ CREATE TABLE solution_blue (
   `user_id` varchar(48) NOT NULL DEFAULT '' COMMENT '比赛代码',
   `contest_code` varchar(40) NOT NULL DEFAULT '' COMMENT '比赛代码',
   `pid` int(11) NOT NULL DEFAULT '0' COMMENT '问题id',
-  `ans` text DEFAULT '' COMMENT '提交答案',
+  `ans` varchar(256) NOT NULL DEFAULT '' COMMENT '提交答案',
   `solution_id` int(11) DEFAULT NULL DEFAULT '0' COMMENT 'ACM模式提交id',
   `score` int(11) NOT NULL DEFAULT '0' COMMENT '分数',
   `is_correct` int(1) DEFAULT '0' DEFAULT '0' COMMENT '答案正确标志',
-  `submit_time` datetime NOT NULL DEFAULT '0000-00-00' COMMENT '提交时间',
+  `submit_time` datetime NOT NULL DEFAULT '1970-01-01' COMMENT '提交时间',
   `type` int(1) NOT NULL DEFAULT '0' COMMENT '提交题目类型',
   `ip` varchar(20) NOT NULL DEFAULT '' COMMENT '提交ip',
   PRIMARY KEY (`submit_id`)
@@ -21,11 +21,11 @@ CREATE TABLE solution_blue (
 
 CREATE TABLE `contest_blue` (
   `contest_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '比赛自增id',
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '比赛名称',
-  `start_time` datetime NOT NULL DEFAULT '0000-00-00' COMMENT '比赛开始时间',
-  `end_time` datetime NOT NULL DEFAULT '0000-00-00' COMMENT '比赛结束时间',
+  `title` varchar(256) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '比赛名称',
+  `start_time` datetime NOT NULL DEFAULT '1970-01-01' COMMENT '比赛开始时间',
+  `end_time` datetime NOT NULL DEFAULT '1970-01-01' COMMENT '比赛结束时间',
   `defunct` char(1) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N' COMMENT '废弃标志',
-  `description` text COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '比赛描述',
+  `description` varchar(256) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '比赛描述',
   `lang` int(1) NOT NULL DEFAULT '0' COMMENT '比赛语言',
   `password` char(16) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '比赛密码',
   PRIMARY KEY (`contest_id`)
@@ -34,7 +34,7 @@ CREATE TABLE `contest_blue` (
 CREATE TABLE `contest_blue_problem` (
   `problem_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '比赛题目自增id',
   `contest_id` int(11) NOT NULL DEFAULT '0' COMMENT '比赛id',
-  `title` char(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '',
+  `title` varchar(256) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '',
   `type` char(1) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目类型',
   `score` int(11) NOT NULL DEFAULT '0' COMMENT '题目分值',
   `num` int(11) NOT NULL DEFAULT '0' COMMENT '题目序号',
@@ -43,10 +43,10 @@ CREATE TABLE `contest_blue_problem` (
 
 CREATE TABLE `problem_fillblank` (
   `problem_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '填空题自增id',
-  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目名称',
-  `description` text COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目描述',
-  `solution` text COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目正确答案',
-  `hint` text COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目提示',
+  `title` varchar(256) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目名称',
+  `description` text COLLATE utf8_unicode_ci NOT NULL COMMENT '题目描述',
+  `solution` varchar(1024) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目正确答案',
+  `hint` varchar(2048) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '题目提示',
   `defunct` char(1) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N' COMMENT '废弃标志',
   PRIMARY KEY (`problem_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT ='蓝桥模式填空题表';
